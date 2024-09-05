bf7d557d092d079c3a5c6c27bdf041977246ee29
Ayush <ayushratan082@gmail.com>
2024-09-05 21:44:12 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Int from "./pages/Int";
 
 import Ques from "./pages/Ques";
+import Socials from "./pages/socials";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Int />} />
           <Route path="/Ques" element={<Ques />} />
+          <Route path="/Socials" element={<Socials />} />
         </Routes>
       </div>
     </Router>
